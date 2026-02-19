adf89d9f20b10280df0a1dcbefb5f2041f4c7bac
GitHub AxdeKlerk <axdeklerk@gmail.com>
2026-02-19 07:16:09 +0000

    Migrate database from SQLite to Neon Postgres Installed psycopg driver Configured django-environ DATABASE_URL handling Conditional SSL for Postgres only Exported and re-imported app data cleanly Verified schema, data integrity and live writes Completed smoke test including payment flow

--- mt_core/core/settings.py
+++ mt_core/core/settings.py
@@ -1,5 +1,5 @@
-
 import os
+from urllib.parse import urlparse
 import environ
 from pathlib import Path
 import cloudinary
@@ -94,17 +94,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
+#DATABASES = {
+#   "default": env.db(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+#}
+
+#DATABASES["default"]["OPTIONS"] = {
+#    "sslmode": "require",
+#}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.db(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 }
+
+if DATABASES["default"]["ENGINE"] == "django.db.backends.postgresql":
+    DATABASES["default"]["OPTIONS"] = {
+        "sslmode": "require",
+    }
+
 
 
 # Password validation
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
