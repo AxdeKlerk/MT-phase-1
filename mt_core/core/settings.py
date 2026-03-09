913628f4f9891b439593403a6c8d8d9a8dc6189e
GitHub AxdeKlerk <axdeklerk@gmail.com>
2026-03-09 07:56:34 +0000

    Correct Allowed Hosts to include heroku for deployment

--- mt_core/core/settings.py
+++ mt_core/core/settings.py
@@ -45,7 +45,7 @@ STRIPE_WEBHOOK_SECRET = env("STRIPE_WEBHOOK_SECRET")
 # #SQUARE_SIGNATURE_KEY = os.getenv("SQUARE_SIGNATURE_KEY")
 # #SQUARE_BASE_URL = os.getenv("SQUARE_BASE_URL", "https://connect.squareupsandbox.com")
 
-ALLOWED_HOSTS = "ALLOWED_HOSTS", default=["localhost", '127.0.0.1', "moshtip.com", "www.moshtip.com"]
+ALLOWED_HOSTS = ["localhost", '127.0.0.1', "moshtip.com", "www.moshtip.com"]
 
 # env.list("ALLOWED_HOSTS", default=["localhost", '127.0.0.1', "moshtip.com", "www.moshtip.com"])
 
