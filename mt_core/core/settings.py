d6646f060969b996cec10a138bcde1d020ba3d5c
GitHub AxdeKlerk <axdeklerk@gmail.com>
2026-03-09 07:53:50 +0000

    Fix allowed hosts for production domain

--- mt_core/core/settings.py
+++ mt_core/core/settings.py
@@ -45,9 +45,11 @@ STRIPE_WEBHOOK_SECRET = env("STRIPE_WEBHOOK_SECRET")
 # #SQUARE_SIGNATURE_KEY = os.getenv("SQUARE_SIGNATURE_KEY")
 # #SQUARE_BASE_URL = os.getenv("SQUARE_BASE_URL", "https://connect.squareupsandbox.com")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", '127.0.0.1', "moshtip.com", "www.moshtip.com"])
+ALLOWED_HOSTS = "ALLOWED_HOSTS", default=["localhost", '127.0.0.1', "moshtip.com", "www.moshtip.com"]
 
-CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=["https://moshtip.com", "https://www.moshtip.com"])
+# env.list("ALLOWED_HOSTS", default=["localhost", '127.0.0.1', "moshtip.com", "www.moshtip.com"])
+
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=["https://moshtip.com", "https://www.moshtip.com", ".herokuapp.com"])
 
 
 # Application definition
