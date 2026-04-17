a0ebfd168bbc52fd0dd202dbb341f8769c4f6174
GitHub AxdeKlerk <axdeklerk@gmail.com>
2026-04-17 08:45:27 +0100

    Change 'unknown' in scanEvent FORMAT_CHOICES to camel case

--- mt_core/gigs/models.py
+++ mt_core/gigs/models.py
@@ -99,7 +99,7 @@ class ScanEvent(models.Model):
     FORMAT_CHOICES = [
         ("poster", "Poster"),
         ("card", "Card"),
-        ("unknown", "unknown"),
+        ("unknown", "Unknown"),
     ]
 
     gig = models.ForeignKey(
